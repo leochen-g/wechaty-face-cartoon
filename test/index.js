6f6aa72d65a22150ebc231c8b22680cf757633b7
chengeng <chengeng@bloggeng.com>
2022-08-01 19:28:55 +0800

    fix(模块): 升级版本

--- test/index.js
+++ test/index.js
@@ -1,9 +1,13 @@
-const { Wechaty } = require('wechaty')
+const {WechatyBuilder} = require('wechaty')
 const WechatyFaceCartonPlugin = require('../src/index')
 const name = 'wechat-carton'
-const bot = new Wechaty({ name, puppet: 'wechaty-puppet-wechat' })
-
-const processedMessage = {}
+const bot = WechatyBuilder.build({
+  name, // generate xxxx.memory-card.json and save login data for the next login
+  puppet: 'wechaty-puppet-wechat',
+  puppetOptions: {
+    uos: true
+  }
+})
 
 async function onMessage(msg) {
   console.log('msgid', msg.id)
@@ -23,5 +27,5 @@ bot
       tipsword: '卡通', // 私聊发送消息，触发照片卡通化提示 如果直接发送图片，默认进入图片卡通化功能，不填则当用户初次发送文字消息时不做任何处理
     })
   )
-  .start()
+bot.start()
   .catch((e) => console.error(e))
